08e0ba6bd6d2a509388ae2e8906908476868bc92
Pramod Biligiri <pramodbiligiri@users.noreply.github.com>
2025-02-20 15:35:14 +0530

    docs

---
+++ README.md
@@ -0,0 +1,4 @@
+paper-rss
+=========
+Generates RSS feeds for recent paper data in the database. Can be used to generate
+topic specific podcasts
--- ops/prod/deploy-het.sh
+++ ops/prod/deploy-het.sh
@@ -6,7 +6,7 @@ newdir=`ssh -i ~/.ssh/id_het rssapp@new.papertime.app 'bin/new-build-dir'`;
 
 echo "New build dir: $newdir";
 
-scp -i ~/.ssh/id_het target/tts-rss-1.0-SNAPSHOT.jar $LOGIN_USER@new.papertime.app:$newdir
+scp -i ~/.ssh/id_het target/paper-rss-1.0-SNAPSHOT.jar $LOGIN_USER@new.papertime.app:$newdir
 
 remote_cmd="bin/update-build $newdir";
 
